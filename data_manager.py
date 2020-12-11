c049ca67a4616c88e92d9247f5829cf4aa1d0a0a
Richard Goshen <rick.goshen@gmail.com>
2020-12-11 11:07:43 -0700

    added get method for prices google sheet

--- data_manager.py
+++ data_manager.py
@@ -1,3 +1,21 @@
+import requests
+from pprint import pprint
+
+SHEETY_PRICES_ENDPOINT = "https://api.sheety.co/66b68d8e72da89ec872ba57ded11c330/flightDeals/prices"
+
 class DataManager:
-    #This class is responsible for talking to the Google Sheet.
-    pass
+    """This class is responsible for talking to the Google Sheet."""
+    def __init__(self):
+        self.destination_data = {}
+
+    def get_destination_data(self):
+        response = requests.get(url=SHEETY_PRICES_ENDPOINT)
+        destination_data = response.json()['prices']
+        # pprint(destination_data)
+        return destination_data
+
+#TODO: POST(add rows) sheet:prices data
+
+#TODO: PUT(update rows) sheet:prices data
+
+#TODO: DELETE sheet:prices data
--- main.py
+++ main.py
@@ -1,1 +1,10 @@
-#This file will need to use the DataManager,FlightSearch, FlightData, NotificationManager classes to achieve the program requirements.
+from data_manager import DataManager
+from pprint import pprint
+
+data_manager = DataManager()
+sheet_data = data_manager.get_destination_data()
+# pprint(sheet_data)
+
+for row in sheet_data:
+    if row["iataCode"] == "":
+        print(f"{row['id']} has no iata code")
